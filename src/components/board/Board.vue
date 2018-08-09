eb622f7ac0e463e7663ed3089e16f295fa90b671
Phil Johnstone <phil@lemonshark.ltd.uk>
2018-08-09 09:54:18 +0100

    simplified css in header

--- src/components/board/Board.vue
+++ src/components/board/Board.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="row">
     <div class="col-sm" v-for="(column, index) in value" :column="column" :key="column.name">
-      <p class="column-title" style="text-align: center">
-        <span class="fa fa-cog" style="float: left; cursor: pointer;" @click="setup" title="Setup Column"></span>
-        {{ column.name }}
-        <span class="fa fa-plus" style="float: right; cursor: pointer;" @click="add" title="Add Column"></span>
-      </p>
-      <draggable :list="column.tasks" :options="{ group:'tasks' }" style="min-height: 100px">
+      <draggable :list="column.tasks" :options="{ group:'tasks' }" class="column">
+        <p class="column-header">
+          <span class="fa fa-cog" style="float: left" @click="setup" title="Setup Column"></span>
+          {{ column.name }}
+          <span class="fa fa-plus" style="float: right" @click="add" title="Add Column"></span>
+        </p>
         <Task v-for="(task, index) in column.tasks" :key="task.name" v-model="column.tasks[index]"></Task>
       </draggable>
     </div>
@@ -34,16 +34,20 @@ export default {
 </script>
 
 <style>
-.col-sm, .row {
+.col-sm, .row, .column {
   height: 100%;
 }
 .col-sm {
-  border-left: 1rem solid #EFEFEF;
+  border-left: 0.5rem solid #EFEFEF;
+  padding-top: 1rem;
 }
 .col-sm:first-child {
   border-left: 0;
 }
-.column-title {
-  padding-top: 1rem;
+.column-header {
+  text-align: center;
+}
+.fa {
+  cursor: pointer;
 }
 </style>
